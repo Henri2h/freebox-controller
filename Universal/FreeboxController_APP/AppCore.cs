06b2a756a4c2f859d84fe2e1d3637fca8e21a9c3
Henri2h <henri.carnot@hotmail.fr>
2017-04-22 00:10:19 +0200

    Added an AppCore in the universall app

---
+++ Universal/FreeboxController_APP/AppCore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeboxController_APP
+{
+  public   class AppCore
+    {
+        public static CodeShared.FreeboxControl FreeboxController { get; set; }
+        public static Windows.Storage.StorageFolder AppStorageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+        public static string AppDataFolder { get { return Path.Combine(AppStorageFolder.Path, "Data"); } }
+
+        public static async Task StartAsync()
+        {
+            FreeboxController = new CodeShared.FreeboxControl("http://mafreebox.freebox.fr", AppDataFolder);
+
+            if (FreeboxController.authentification.NeedRegistration)
+            {
+                System.Diagnostics.Debug.WriteLine("Registering app");
+                await FreeboxController.authentification.RegisterAppAsync();
+            }
+
+            System.Diagnostics.Debug.WriteLine("App registered");
+            FreeboxController.authentification.ConnectAppAsync();
+
+            System.Diagnostics.Debug.WriteLine("END");
+        }
+    }
+}
