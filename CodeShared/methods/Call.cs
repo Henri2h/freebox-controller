4472e3e8316253e4f06a016403cc557f3aa3193b
Henri2h <henri.carnot@hotmail.fr>
2017-04-25 19:33:03 +0200

    added Call listing

---
+++ CodeShared/methods/Call.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeShared.methods
+{
+    public class Call
+    {
+        public async Task<List<CallEntry>> GetCallsAsync()
+        {
+            string JsonResponse = await HTTP_Request.HTTP_GETAsync(Core.Host, "/api/v3/call/log/", null);
+            JObject response = JObject.Parse(JsonResponse);
+
+            bool success = (bool)response["success"];
+            if (success)
+            {
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<List<CallEntry>>(response["result"].ToString());
+            }
+            return null;
+        }
+
+    }
+    public class CallEntry
+    {
+        public string number { get; set; }
+        public string type { get; set; }
+        public int id { get; set; }
+        public int duration { get; set; }
+        public int datetime { get; set; }
+        public int contact_id { get; set; }
+        public int line_id { get; set; }
+        public string name { get; set; }
+        public bool NewCall { get; set; }
+    }
+}
